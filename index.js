964032cad4a490d75340b26b768e549dbfe08dfd
aldusd <aldusdanielfsm@gmail.com>
2022-06-17 11:36:20 -0300

    wakatime

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-import express from
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const users = [
+
+];
+
+const tweets = [
+
+];
+
+app.post("/sign-up", (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(5000);
