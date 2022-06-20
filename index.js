6c227e346168e13781251081555d667ada1c4568
aldusd <aldusdanielfsm@gmail.com>
2022-06-20 09:54:11 -0300

    install sign-up

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const users = [
+// Data
+const users = [];
+const tweets = [];
 
-];
-
-const tweets = [
-
-];
+// Functions
+const validateUser = (user) => {
+    for(let i = 0; i < users.length; i++) {
+        if(user.username === users[i].username) return false;
+    }
+    return true;
+}
 
 app.post("/sign-up", (req, res) => {
-    console.log(req.body)
+    const user = req.body;
+    if (validateUser(user)) {
+        users.push(user);
+        res.send('Ok');
+    }
+    res.send("Invalid Data"); // Status code 422?
+})
+
+app.get("/test", (req, res) => {
+    console.log(users, tweets);
+    res.send('Test Done')
 })
 
 app.listen(5000);
